21ee27ddd5a76ee78e62b0b276e649f6608e9e4e
JJong-Min <sambujakkk@naver.com>
2021-08-07 22:55:14 +0900

    [Correct] 2869. 달팽이는 올라가고 싶다
    백준 알고리즘 2557번 문제 풀이 코드입니다.
    다른 풀이 추가.

--- [Week01]Alogorithm1/기초, 배열, 문자열, 재귀함수/2869.달팽이는 올라가고 싶다.py
+++ [Week01]Alogorithm1/기초, 배열, 문자열, 재귀함수/2869.달팽이는 올라가고 싶다.py
@@ -1,5 +1,5 @@
 import sys
-
+# 내 풀이
 A, B, V = map(int, (sys.stdin.readline().split()))
 before_last_day_height = V - A
 days = 1
@@ -9,3 +9,12 @@ if before_last_day_height % (A - B) == 0:
 
 else:
     days += before_last_day_height // (A - B) + 1
+
+# 다른 사람 풀이
+#if -else가 아닌 math.ceil을 사용
+
+import math
+A, B, V = map(int, input().split())
+# print(A, B, V)
+day_count = (V - B) / (A - B)
+print(math.ceil(day_count))
