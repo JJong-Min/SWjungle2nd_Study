7b765ccdf6cc45280bfe6f9b66342f22e3d2229c
JJong-Min <sambujakkk@naver.com>
2021-08-08 23:14:48 +0900

    Create shell sort function
    셸 정렬 알고리즘을 구현한 코드입니다.

---
+++ [Week01]Alogorithm1/기초, 배열, 문자열, 재귀함수/정렬/shell_sort_func.py
@@ -0,0 +1,12 @@
+def shell_sort(arr):
+    length_arr = len(arr)
+    h = length_arr // 2
+    while h > 0:
+        for i in range(h, n):
+            j = i - h
+            tmp = a[i]
+            while j >= 0 and a[j] > tmp:
+                a[j + h] = a[j]
+                j -= h
+            a[j + h] = tmp
+        h //= 2
