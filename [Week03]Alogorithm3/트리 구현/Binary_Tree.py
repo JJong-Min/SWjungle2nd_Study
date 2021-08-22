55488afe5630a1611ac722eaf35d58857cff875a
JJong-Min <sambujakkk@naver.com>
2021-08-22 23:20:49 +0900

    이진 트리를 구현한 코드입니다.

---
+++ [Week03]Alogorithm3/트리 구현/Binary_Tree.py
@@ -0,0 +1,34 @@
+# 이진 트리
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+    
+    def __str__(self):
+        return str(self.data)
+
+class Tree:
+    def __init__(self):
+        self.root = None
+
+    def preorderTraversal(self, node):
+        print(node, end='')
+        if not node.left  == None : self.preorderTraversal(node.left)
+        if not node.right == None : self.preorderTraversal(node.right)
+
+    def inorderTraversal(self, node):
+        if not node.left  == None : self.inorderTraversal(node.left)
+        print(node, end='')
+        if not node.right == None : self.inorderTraversal(node.right)
+    
+    def postorderTraversal(self, node):
+        if not node.left  == None : self.postorderTraversal(node.left)
+        if not node.right == None : self.postorderTraversal(node.right)
+        print(node, end='')
+
+    def makeRoot(self, node, left_node, right_node):
+        if self.root == None:
+            self.root = node
+        node.left = left_node
+        node.right = right_node
