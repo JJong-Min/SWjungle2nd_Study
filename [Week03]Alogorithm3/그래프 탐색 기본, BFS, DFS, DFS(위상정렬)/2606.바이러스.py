c624ff59906bd24a767b7cf775d3785c191cb034
JJong-Min <sambujakkk@naver.com>
2021-08-25 19:38:28 +0900

    [Correct] 2606. 바이러스

--- [Week03]Alogorithm3/그래프 탐색 기본, BFS, DFS, DFS(위상정렬)/2606.바이러스.py
+++ [Week03]Alogorithm3/그래프 탐색 기본, BFS, DFS, DFS(위상정렬)/2606.바이러스.py
@@ -1,3 +1,5 @@
+# bfs
+
 import sys
 from collections import deque
 
@@ -23,3 +25,29 @@ while queue:
             queue.append(i)
 
 print(cnt - 1)
+
+# dfs
+import sys
+
+def dfs(i):
+    cnt = 0
+    for j in graph[i]:
+        if check[j]:
+            check[j] = False
+            cnt += 1
+            cnt += dfs(j)
+
+    return cnt
+
+n = int(sys.stdin.readline())
+m = int(sys.stdin.readline())
+
+graph = [[] for _ in range(n + 1)]
+for _ in range(m):
+    x, y = map(int, sys.stdin.readline().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+check = [True for _ in range(n + 1)]
+check[1] = False
+print(dfs(1))
