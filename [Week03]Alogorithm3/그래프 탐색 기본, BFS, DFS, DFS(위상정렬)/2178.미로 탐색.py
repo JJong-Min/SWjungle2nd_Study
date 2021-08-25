cc1b6c43e7540417a6db4e3c53b5fa60815c21f1
JJong-Min <sambujakkk@naver.com>
2021-08-25 19:38:56 +0900

    [Correct] 25178. 미로 탐색

--- [Week03]Alogorithm3/그래프 탐색 기본, BFS, DFS, DFS(위상정렬)/2178.미로 탐색.py
+++ [Week03]Alogorithm3/그래프 탐색 기본, BFS, DFS, DFS(위상정렬)/2178.미로 탐색.py
@@ -1,26 +1,24 @@
 import sys
 from collections import deque
 
-def bfs(i, j):
-    queue = deque([[i, j]])
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, 1, -1]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, 1, -1]
+
+def bfs(x, y):
+    queue = deque([[x, y]])
     while queue:
         x, y = queue.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < N and 0 <= ny < M and graph[nx][ny] == 1:
+            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 1:
                 graph[nx][ny] += graph[x][y]
-                queue.append([nx, ny])    
-    
-                
-        
-N, M = map(int, sys.stdin.readline().split())
-graph = [[] for _ in range(N)]
-for i in range(N):
-    input_val = sys.stdin.readline().rstrip()
-    for j in input_val:
-        graph[i].append(int(j))
+                queue.append([nx, ny])
+
+n, m = map(int, sys.stdin.readline().split())
+graph = [list(map(int, sys.stdin.readline().rstrip())) for _ in range(n)]
 bfs(0, 0)
-print(graph[N-1][M-1])
+
+print(graph[n - 1][m - 1])
+
+
