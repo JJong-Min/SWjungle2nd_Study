613dd00b8efb70bb8ba8422c08156a949bd8538f
JJong-Min <sambujakkk@naver.com>
2021-10-07 08:52:02 +0900

    [재풀이] 파일명 정렬

--- [Week05~]Additional PS/[프로그래머스] 파일명 정렬.py
+++ [Week05~]Additional PS/[프로그래머스] 파일명 정렬.py
@@ -1,3 +1,27 @@
+# 2021-10-07(재풀이 완료)
+def solution(files):
+    answer = []
+    #파일명 parsing
+    for file in files:
+        head, number, tail = '', '', ''
+        check = False
+        for i in range(len(file)):
+            if file[i].isdigit():
+                number += file[i]
+                check = True
+            
+            elif not check:
+                head += file[i]
+            
+            else:
+                tail += file[i:]
+                break
+        answer.append((head, number, tail))
+    answer.sort(key=lambda x:(x[0].upper(), int(x[1])))
+    real_answer = [''.join(i) for i in answer]        
+    return real_answer
+
+#2021-10-06(해답보고 풀이)
 def solution(files):
     new_files = []
     for file in files:
