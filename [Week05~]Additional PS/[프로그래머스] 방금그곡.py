688cce339657cdd7a0735838e5dd6718abba8738
JJong-Min <sambujakkk@naver.com>
2021-10-08 09:52:09 +0900

    [재풀이] 방금그곡

--- [Week05~]Additional PS/[프로그래머스] 방금그곡.py
+++ [Week05~]Additional PS/[프로그래머스] 방금그곡.py
@@ -1,57 +1,33 @@
-import heapq
-def check_m(m, i, play_song):
-    if len(play_song[i:]) < len(m):
-        val_song = play_song[i:] + play_song[:len(m) - len(play_song[i:])]
-    else:
-        val_song = play_song[i:len(m)]
-
-    for um, mm in zip(val_song, m):
-        if um != mm:
-            return False
-    else:
-        return True
-
-def change_shap(strings):
-    if 'C#' in strings:
-        strings = strings.replace('C#', 'c')
-    if 'D#' in strings:
-        strings = strings.replace('D#', 'd')
-    if 'F#' in strings:
-        strings = strings.replace('F#', 'f')
-    if 'G#' in strings:
-        strings = strings.replace('G#', 'g')
-    if 'A#' in strings:
-        strings = strings.replace('A#', 'a')
-    return strings
+def change_shap(music):
+    music = music.replace('C#', 'c')
+    music = music.replace('D#', 'd')
+    music = music.replace('F#', 'f')
+    music = music.replace('G#', 'g')
+    music = music.replace('A#', 'a')
+    return music
 
 def solution(m, musicinfos):
-    heap = []
-    heapq.heapify(heap)
-    musicinfos = [musicinfo.split(',') for musicinfo in musicinfos]
+    available_musicinfos = []
     m = change_shap(m)
-    cnt = 0
-    for musicinfo in musicinfos:
-        cnt += 1
-        play_time = (int(musicinfo[1][:2]) - int(musicinfo[0][:2])) * 60 + (int(musicinfo[1][3:]) - int(musicinfo[0][3:]))
-        play_song = ''
-        if len(musicinfo[3]) >= play_time:
-            play_song = musicinfo[3][:play_time]
+    # 전처리 과정
+    for i, musicinfo in enumerate(musicinfos):
+        start_time, end_time, title, music = musicinfo.split(',')
+        music = change_shap(music)
+        start_hour, start_minute = start_time.split(':')
+        end_hour, end_minute = end_time.split(':')
+        play_time = (int(end_hour) - int(start_hour)) * 60 + (int(end_minute) - int(start_minute))
+        
+        if len(music) >= play_time:
+            music = music[:play_time]
         else:
-            t = (play_time - len(musicinfo[3])) // len(musicinfo[3])
-            r = (play_time - len(musicinfo[3])) % len(musicinfo[3])
-            play_song = musicinfo[3] + musicinfo[3] * t + musicinfo[3][:r]
-
-        for i in range(len(play_song)):
-            if m[0] == play_song[i] and check_m(m, i, play_song):
-                heapq.heappush(heap, (-play_time, cnt, musicinfo[2]))
-                break
-
-    if heap:
-        return heapq.heappop(heap)[2]   
-    else:
-        return None
-
-m = 'CC#BCC#BCC#BCC#B'
-musicinfos = ["03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"]
-
-print(solution(m, musicinfos))
+            music = music * (play_time // len(music)) + music[:play_time % len(music)]
+        
+        if m in music: 
+            available_musicinfos.append((play_time, i, title))
+    
+    if available_musicinfos:
+        available_musicinfos = sorted(available_musicinfos, key = lambda x: (-x[0], x[1]))
+        return available_musicinfos[0][2]
+    
+    return  "(None)"
+        
