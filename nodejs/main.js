3c03f22acf6f0ba3dc2bebe98027c5ddb5bb9186
JJong-Min <sambujakkk@naver.com>
2021-10-24 21:33:04 +0900

    form을 이용한 create

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -33,6 +34,7 @@ var app = http.createServer(function(request,response){
           <body>
             <h1><a href="/">WEB</a></h1>
             ${list}
+            <a href="/create">create</a>
             <h2>${title}</h2>
             <p>${description}</p>
           </body>
@@ -42,6 +44,55 @@ var app = http.createServer(function(request,response){
           response.end(template);
         })
       });
+  } else if (pathname === '/create') {
+    fs.readdir('./data', function(error, filelist){
+      var list = `<ul>`;
+      var i = 0;
+      while(i < filelist.length) {
+        list += `<li> <a href="/?id=${filelist[i]}"> ${filelist[i]}</a> </li>`;
+        i += 1;  
+      }
+      list += `</ul>`
+      var template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - Create</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        ${list}
+        <form action="http://localhost:3000/create_process" method="post">
+          <p><input type="text" name="title" placeholder="title"></p>
+          <p>
+            <textarea name="description" placeholder="description"></textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+      </body>
+      </html>
+      `;   
+      response.writeHead(200);
+      response.end(template);
+    });
+
+  } else if (pathname === '/create_process'){
+    var body = '';
+    request.on('data', function(data) {
+      body += data;
+    });
+    request.on('end', function() {
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+      fs.writeFile(`data/${title}`, description, 'utf-8', function(error){
+        response.writeHead(302, {location: `/?id=${title}`});
+        response.end('success');
+      });
+    });
+
   } else {
     response.writeHead(404);
     response.end('Not Found');
