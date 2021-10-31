e1e9a1cee218f1037c8f7edd516b5ed78ea78397
JJong-Min <sambujakkk@naver.com>
2021-10-31 16:29:00 +0900

    middleware 선언 후 사용해보기

--- nodejs/Express/main.js
+++ nodejs/Express/main.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const app = express();
+
+const mw = (req, res, next) => {
+  console.log('mw!');
+  next();
+}
+
+app.use(mw);
 app.listen(3000, () => console.log('running'));
