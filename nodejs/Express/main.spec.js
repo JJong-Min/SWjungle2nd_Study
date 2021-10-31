29731a870c9975aa9d5805072d0bd5b3f62cca52
JJong-Min <sambujakkk@naver.com>
2021-10-31 19:38:57 +0900

    supertest모듈을 이용한 통합테스트

--- nodejs/Express/main.spec.js
+++ nodejs/Express/main.spec.js
@@ -1,7 +1,18 @@
 const assert = require('assert');
+const should = require('should');
+const request = require('supertest');
+const app = require('./main');
 
 describe('GET /users', () => {
-    it('배열을 반환한다.', () => {
-        assert.equal(1, 1);
+    it('배열을 반환한다.', (done) => {
+        request(app)
+            .get('/users')
+            .end((err, res) => {
+                res.body.should.be.instanceOf(Array);
+                res.body.forEach(user => {
+                    user.should.have.property('name');
+                });
+                done();
+            });
     });
 })
