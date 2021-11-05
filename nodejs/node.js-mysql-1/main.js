8107151d4dc639d8818aaa2df894751ab644919b
JJong-Min <sambujakkk@naver.com>
2021-11-06 00:28:43 +0900

    C:/Program Files/Git/create, /create_process  구현

--- nodejs/node.js-mysql-1/main.js
+++ nodejs/node.js-mysql-1/main.js
@@ -6,12 +6,16 @@ var template = require('./lib/template.js');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
 var mysql      = require('mysql');
+const { title } = require('process');
+
+/*
 var db = mysql.createConnection({
   host     : '-',
   user     : '-',
   password : '-',
   database : '-'
 });
+*/
 db.connect();
 
 var app = http.createServer(function(request,response){
@@ -33,28 +37,6 @@ var app = http.createServer(function(request,response){
           response.end(html);
         });
       } else {
-        // fs.readdir('./data', function(error, filelist){
-        //   var filteredId = path.parse(queryData.id).base;
-        //   fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-        //     var title = queryData.id;
-        //     var sanitizedTitle = sanitizeHtml(title);
-        //     var sanitizedDescription = sanitizeHtml(description, {
-        //       allowedTags:['h1']
-        //     });
-        //     var list = template.list(filelist);
-        //     var html = template.HTML(sanitizedTitle, list,
-        //       `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        //       ` <a href="/create">create</a>
-        //         <a href="/update?id=${sanitizedTitle}">update</a>
-        //         <form action="delete_process" method="post">
-        //           <input type="hidden" name="id" value="${sanitizedTitle}">
-        //           <input type="submit" value="delete">
-        //         </form>`
-        //     );
-        //     response.writeHead(200);
-        //     response.end(html);
-        //   });
-        // });
         db.query(`SELECT * FROM topic`, function(error, topics){
           if (error) {
             throw error;
@@ -81,9 +63,12 @@ var app = http.createServer(function(request,response){
         });
       }
     } else if(pathname === '/create'){
-      fs.readdir('./data', function(error, filelist){
+      db.query(`SELECT * FROM topic`, function(error, topics){
+        if (error) {
+          throw error;
+        }
         var title = 'WEB - create';
-        var list = template.list(filelist);
+        var list = template.list(topics);
         var html = template.HTML(title, list, `
           <form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
@@ -105,12 +90,18 @@ var app = http.createServer(function(request,response){
       });
       request.on('end', function(){
           var post = qs.parse(body);
-          var title = post.title;
-          var description = post.description;
-          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-            response.writeHead(302, {Location: `/?id=${title}`});
-            response.end();
-          })
+          db.query(`
+            INSERT INTO topic (title, description, created, author_id)
+              VALUES (?, ?, NOW(), ?)`,
+            [post.title, post.description, 1],
+            function (error, result){
+              if (error){
+                throw error
+              };
+              response.writeHead(302, {Location: `/?id=${result.insertId}`});
+              response.end();
+
+            })
       });
     } else if(pathname === '/update'){
       fs.readdir('./data', function(error, filelist){
