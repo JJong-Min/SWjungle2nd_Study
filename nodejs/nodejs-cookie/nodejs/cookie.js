f6bafa586c1ef0639e48d2ee13d2fd0be0a4683d
JJong-Min <sambujakkk@naver.com>
2021-11-06 18:00:49 +0900

    permanent cookie 만들기

--- nodejs/nodejs-cookie/nodejs/cookie.js
+++ nodejs/nodejs-cookie/nodejs/cookie.js
@@ -9,7 +9,11 @@ http.createServer(function(request, response){
     }
     
     response.writeHead(200, {
-        'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry']
-    })
+        'Set-Cookie': [
+            'yummy_cookie=choco', 
+            'tasty_cookie=strawberry',
+            `Permanent=cookies; Max-Age=${60*60*24*30}`
+        ]
+    });
     response.end('Cookie!!');
 }).listen(3000); 
