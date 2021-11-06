0865ab4059c44367d89a20697239b46eed8b6159
JJong-Min <sambujakkk@naver.com>
2021-11-06 17:56:31 +0900

    쿠키 읽기

--- nodejs/nodejs-cookie/nodejs/cookie.js
+++ nodejs/nodejs-cookie/nodejs/cookie.js
@@ -1,7 +1,15 @@
 var http = require('http');
+var cookie = require('cookie');
+
 http.createServer(function(request, response){
+    // parse는 undifined를 만나면 고장남(예외처리)
+    var cookies = {};
+    if (request.headers.cookie !== undefined) {
+        cookies = cookie.parse(request.headers.cookie);
+    }
+    
     response.writeHead(200, {
         'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry']
     })
     response.end('Cookie!!');
-}).listen(3000);
+}).listen(3000); 
