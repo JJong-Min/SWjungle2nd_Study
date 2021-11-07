209a49d6ca15dcf322cf80fc03161c764a0ec4fa
JJong-Min <sambujakkk@naver.com>
2021-11-07 17:44:49 +0900

    passport 모듈 사용해서 로그인/로그아웃

--- nodejs/nodejs-session/main.js
+++ nodejs/nodejs-session/main.js
@@ -19,6 +19,61 @@ app.use(session({
   store:new FileStore()
 }))
 
+var authData = {
+  email: 'egoing@gmail.com',
+  password: '1111',
+  nickname: 'egoing'
+}
+
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  console.log('serializeUser', user);
+  done(null, user.email);
+});
+
+passport.deserializeUser(function(id, done) {
+  console.log('deserializeUser', id);
+  done(null, authData);
+});
+
+passport.use(new LocalStrategy(
+  {
+    usernameField: 'email',
+    passwordField: 'pwd'
+  },
+  function(username, password, done) {
+    console.log('LocalStrategy', username, password);
+    
+    if(username === authData.email) {
+      console.log('1');
+      if(password === authData.password) {
+        console.log('2');
+        return done(null, authData);
+      } else {
+        console.log('3');
+        return done(null, false, {
+          message: 'Incorrect password.'
+        });
+      }
+    } else {
+      console.log('4');
+      return done(null, false, {
+        message: 'Incorrect username.'
+      });
+    }
+  }
+));
+
+app.post('/auth/login_process',
+passport.authenticate('local', { successRedirect: '/',
+                                   failureRedirect: '/auth/login' }));
+
+
 app.get('*', function(request, response, next){
   fs.readdir('./data', function(error, filelist){
     request.list = filelist;
