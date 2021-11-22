78fb4e912a0e1ff9d9f046eef99383b98a60166c
Ratel <sambujakkk@naver.com>
2021-11-23 00:51:05 +0900

    add category radio box

--- nodejs/namu_Test/client/NewChallenge/index.js
+++ nodejs/namu_Test/client/NewChallenge/index.js
@@ -102,50 +102,65 @@ export default function NewAlien(props) {
   return (
     <div class="flex justify-center items-center w-full bg-blue-400">
     <div class="w-1/2 bg-white rounded shadow-2xl p-8 m-4">
-        <h1 class="block w-full text-center text-gray-800 text-2xl font-bold mb-6">Component Form</h1>
+        <h1 class="block w-full text-center text-gray-800 text-2xl font-bold mb-6">새로운 챌린지 생성</h1>
         <form action="/" method="post">
             <div class="flex flex-col mb-4">
                 <label class="mb-2 font-bold text-lg text-gray-900" for="first_name">First Name</label>
                 <input class="border py-2 px-3 text-grey-800" type="text" name="first_name" id="first_name" />
             </div>
             <div class="flex flex-col mb-4">
-                <label class="mb-2 font-bold text-lg text-gray-900" for="last_name">Last Name</label>
-                <input class="border py-2 px-3 text-grey-800" type="text" name="last_name" id="last_name" />
+                <label class="mb-2 font-bold text-lg text-gray-900" for="textarea">textarea</label>
+                <textarea class="border py-2 px-3 text-grey-800" name="textarea" id="textarea"></textarea>
             </div>
-            <div class="flex flex-col mb-4">
-                <label class="mb-2 font-bold text-lg text-gray-900" for="email">Email</label>
-                <input class="border py-2 px-3 text-grey-800" type="email" name="email" id="email" />
-            </div>
-            <div class="flex flex-col mb-4">
-                <label class="mb-2 font-bold text-lg text-gray-900" for="password">Password</label>
-                <input class="border py-2 px-3 text-grey-800" type="password" name="password" id="password" />
-            </div>
-            <div class="flex flex-col mb-4">
-                <label class="mb-2 font-bold text-lg text-gray-900" for="Date">Date</label>
-                <input class="border py-2 px-3 text-grey-800" type="date" name="date" id="date" />
-            </div>
-            <div class="flex flex-col mb-4">
-                <label class="mb-2 font-bold text-lg text-gray-900" for="File">File</label>
-                <input class="border py-2 px-3 text-grey-800" type="file" name="file" id="file" />
-            </div>
+
             <div class="flex flex-col mb-4">
                 <label class="mb-2 font-bold text-lg text-gray-900" for="color">Range</label>
                 <input class="border py-2 text-grey-800" type="range" name="range" id="range" />
             </div>
-            <div class="flex flex-col mb-4">
-                <label class="mb-2 font-bold text-lg text-gray-900" for="textarea">textarea</label>
-                <textarea class="border py-2 px-3 text-grey-800" name="textarea" id="textarea"></textarea>
-            </div>
+
             <div class="flex flex-col mb-4">
                 <label class="mb-2 font-bold text-lg text-gray-900" for="Select">Select</label>
                 <select class="border py-2 px-3 text-grey-800">
-                    <option>Surabaya</option>
-                    <option>Jakarta</option>
-                    <option>Bandung</option>
-                    <option>Mojokerto</option>
+                <option value={SELECT_DEFAULT}>선택</option>
+                <option value="7">매일 참여</option>
+                <option value="6">주 6회 참여</option>
+                <option value="5">주 5회 참여</option>
+                <option value="4">주 4회 참여</option>
+                <option value="3">주 3회 참여</option>
+                <option value="2">주 2회 참여</option>
+                <option value="1">주 1회 참여</option>
                 </select>
             </div>
-            <button class="block bg-green-400 hover:bg-green-600 text-white uppercase text-lg mx-auto p-4 rounded" type="submit">Save</button>
+
+            <div class="main flex border rounded-full overflow-hidden m-4 select-none">
+              <div class="title py-3 my-auto px-5 bg-blue-500 text-white text-sm font-semibold mr-3">Category</div>
+              <label class="flex radio p-2 cursor-pointer">
+                <input class="my-auto transform scale-125" type="radio" name="sfg" />
+                <div class="title px-2">운동</div>
+              </label>
+
+              <label class="flex radio p-2 cursor-pointer">
+                <input class="my-auto transform scale-125" type="radio" name="sfg" />
+                <div class="title px-2">실생활</div>
+              </label>
+
+              <label class="flex radio p-2 cursor-pointer">
+                <input class="my-auto transform scale-125" type="radio" name="sfg" />
+                <div class="title px-2">공부</div>
+              </label>
+
+              <label class="flex radio p-2 cursor-pointer">
+                <input class="my-auto transform scale-125" type="radio" name="sfg" />
+                <div class="title px-2">독서</div>
+              </label>
+
+              <label class="flex radio p-2 cursor-pointer">
+                <input class="my-auto transform scale-125" type="radio" name="sfg" />
+                <div class="title px-2">기타</div>
+              </label>
+            </div>
+
+            <button class="p-2 pl-5 pr-5 transition-colors duration-700 transform bg-indigo-500 hover:bg-blue-400 text-gray-100 text-lg rounded-lg focus:border-4 border-indigo-300" type="button" onClick={handleSubmit}>챌린지 생성</button>
         </form>
     </div>
 </div>
