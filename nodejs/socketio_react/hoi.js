9c63d757c50d063980a6a32991eaa16397939a3e
Ratel <sambujakkk@naver.com>
2021-11-26 04:19:45 +0900

    aws 테스트용 코드

--- nodejs/socketio_react/hoi.js
+++ nodejs/socketio_react/hoi.js
@@ -1,3 +1,3 @@
 console.log(new Date());
 console.log(new Date().toLocaleString("en-US", {timeZone: "Asia/Seoul"}));
-console.timeLog(new Date(new Date().toLocaleString("en-US", {timeZone: "Asia/Seoul"})));
+console.log(new Date(new Date().toLocaleString("en-US", {timeZone: "Asia/Seoul"})));
