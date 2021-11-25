bd81a2ed14830017677778b01bd8052a04e40213
Ratel <sambujakkk@naver.com>
2021-11-26 03:22:12 +0900

    aws 실행용 js파일

---
+++ nodejs/socketio_react/client/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import "./App.css"
+import Card from "./components/card/Card"
+import Navbar from "./components/navbar/Navbar"
+const App = () => {
+  const [username, setUsername] = useState("")
+  const [user, setUser] = useState("")
+  
+  console.log(user);
+  return <div className="container">
+    {user ? (
+      <>
+        <Navbar/>
+        <Card/>
+        <span className="username">{username}</span>
+      </>
+    ) :(
+    <div className="login">
+      <input 
+        type="text"  
+        placeholder="username" 
+        onChange={(e) => setUsername(e.target.value)}/>
+      <button onClick={() => setUser(username)}>login</button>
+    </div>
+  )}
+  </div>;
+}
+
+export default App;
---
+++ nodejs/socketio_react/client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import "./navbar.css"
+import Notification from "../../img/notification.svg"
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <span className="logo">Lama App</span>
+            <div className="icons">
+                <div className="icon">
+                    <img src={Notification} className="iconImg" alt="" />
+                    <div className="counter">2</div>
+                </div>
+             </div>
+        </div>
+    )
+} 
+
+export default Navbar
