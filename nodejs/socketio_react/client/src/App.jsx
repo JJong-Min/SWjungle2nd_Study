0c122985ebbe3aca51bec29b5e9c1ca8fe8a0715
Ratel <sambujakkk@naver.com>
2021-11-26 21:55:49 +0900

    snotification socket 완성

--- nodejs/socketio_react/client/src/App.jsx
+++ nodejs/socketio_react/client/src/App.jsx
@@ -1,19 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css"
 import Card from "./components/card/Card"
 import Navbar from "./components/navbar/Navbar"
 import {posts} from "./data"
+import {io} from "socket.io-client";
+
 const App = () => {
-  const [username, setUsername] = useState("")
-  const [user, setUser] = useState("")
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState("");
+  const[socket, setSocket] = useState(null);
   
+  useEffect(()=>{
+    const socket = io("http://localhost:5000");
+  },[])
+
+  useEffect(()=>{
+    socket.emit("newUser", user)
+  }, [socket, user]);
+
   console.log(user);
   return ( <div className="container">
     {user ? (
       <>
-        <Navbar/>
+        <Navbar socket={socket}/>
         {posts.map((post) => (
-          <Card key={post.id} post={post} />
+          <Card key={post.id} post={post} socket={socket} user={user}/>
         ))}
         
         <span className="username">{username}</span>
--- nodejs/socketio_react/client/src/components/card/Card.jsx
+++ nodejs/socketio_react/client/src/components/card/Card.jsx
@@ -6,31 +6,52 @@ import Share from "../../img/share.svg";
 import Info from "../../img/info.svg";
 import { useState } from "react";
 
-const Card = ({post}) => {
-    const [liked, setLiked] = useState(false);
+const Card = ({ post, socket, user }) => {
+  const [liked, setLiked] = useState(false);
 
-    const handleNotification = () => {
-        setLiked(true);
-    }
-    return (
-        <div className="card">
-            <div className="info">
-                {/* <img src={post.userImg} alt="" classname="userImg" /> */}
-                <span>{post.fullname}</span>
-            </div>
-            {/* <img src={post.postImg} alt="" classname="postImg" /> */}
-        <div className="interaction">
-            {liked ? (<img src={HeartFilled} alt="" classname="cardIcon" />
-            ) : (
-                <img src={Heart} alt="" classname="cardIcon" onClick={handleNotification}/>
-            )}
-            
-            <img src={Comment} alt="" classname="cardIcon" />
-            <img src={Share} alt="" classname="cardIcon" />
-            <img src={Info} alt="" classname="cardIcon infoIcon" />
-        </div>
-        </div>
-    )
-}
+  const handleNotification = (type) => {
+    type === 1 && setLiked(true);
+    socket.emit("sendNotification", {
+      senderName: user,
+      receiverName: post.username,
+      type,
+    });
+  };
 
-export default Card
+  return (
+    <div className="card">
+      <div className="info">
+        <img src={post.userImg} alt="" className="userImg" />
+        <span>{post.fullname}</span>
+      </div>
+      <img src={post.postImg} alt="" className="postImg" />
+      <div className="interaction">
+        {liked ? (
+          <img src={HeartFilled} alt="" className="cardIcon" />
+        ) : (
+          <img
+            src={Heart}
+            alt=""
+            className="cardIcon"
+            onClick={() => handleNotification(1)}
+          />
+        )}
+        <img
+          src={Comment}
+          alt=""
+          className="cardIcon"
+          onClick={() => handleNotification(2)}
+        />
+        <img
+          src={Share}
+          alt=""
+          className="cardIcon"
+          onClick={() => handleNotification(3)}
+        />
+        <img src={Info} alt="" className="cardIcon infoIcon" />
+      </div>
+    </div>
+  );
+};
+
+export default Card;
