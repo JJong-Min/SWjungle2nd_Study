121e7bc615863c3373f414c0b405bea2fd60bcba
Ratel <sambujakkk@naver.com>
2021-11-26 16:41:33 +0900

    navigation bar 만들기

--- nodejs/socketio_react/client/src/components/card/Card.jsx
+++ nodejs/socketio_react/client/src/components/card/Card.jsx
@@ -1,9 +1,24 @@
-import "./card.css"
+import "./card.css";
+import Heart from "../../img/heart.svg";
+import HeartFilled from "../../img/heartFilled.svg";
+import Comment from "../../img/comment.svg";
+import Share from "../../img/share.svg";
+import Info from "../../img/info.svg";
 
-const Card = () => {
+const Card = ({post}) => {
     return (
         <div className="card">
-            card
+            <div className="info">
+                <img src={post.userImg} alt="" classname="userImg"></img>
+                <span>{post.fullname}</span>
+            </div>
+            <img src={post.postImg} alt="" classname="postImg"></img>
+        <div className="interation">
+        <img src={Heart} alt="" classname="cardIcon"></img>
+        <img src={Comment} alt="" classname="cardIcon"></img>
+        <img src={Share} alt="" classname="cardIcon"></img>
+        <img src={Info} alt="" classname="cardIcon"></img>
+        </div>
         </div>
     )
 }
