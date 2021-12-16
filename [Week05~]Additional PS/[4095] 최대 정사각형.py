935da50d38f5c4695dd5d0638ce9d88bc1c93a89
Ratel <sambujakkk@naver.com>
2021-12-16 20:46:16 +0900

    [correct] 최대 정사각형

---
+++ [Week05~]Additional PS/[4095] 최대 정사각형.py
@@ -0,0 +1,18 @@
+import sys
+
+
+while 1:
+    N,M = map(int,sys.stdin.readline().split())
+    if N==0 and M==0: break
+    square = [list(map(int,input().split())) for _ in range(N)]
+    dp = [[0]*(M+1) for _ in range(N+1)]
+
+    max_length = 0
+    for i in range(1,N+1):
+        for j in range(1,M+1):
+            if square[i-1][j-1]:
+                dp[i][j] = min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])+1
+                if dp[i][j] > max_length:
+                    max_length = dp[i][j]
+    
+    print(max_length)
