3efb4d154e0e39f6974ca575f2808230a437b99e
JJong-Min <sambujakkk@naver.com>
2022-01-05 20:28:11 +0900

    [add soultion] 피보나치 수

--- [Week05~]Additional PS/[2747] 피보나치 수.py
+++ [Week05~]Additional PS/[2747] 피보나치 수.py
@@ -22,3 +22,17 @@ def fib(n):
         for i in range(2, n):
             fiblist.append(fiblist[i - 1] + fiblist[i - 2])
     return fiblist[n - 1]
+
+
+f = [0] * (n + 1)                       # n + 1 만큼 리스트 생성
+def pibo(n):
+    if(f[n]) != 0:                      # f안에 값이 존재한다면
+        return f[n]                     # 재귀함수를 거치지 않고 그대로 출력
+
+    if n < 2: 
+        f[n] = n
+        return f[n]
+    
+    else:
+        f[n] = pibo(n-1) + pibo(n-2)  # f[n]에 저장
+    return f[n]
