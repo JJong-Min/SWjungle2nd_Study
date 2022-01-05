de028a3c091047f8c0404e17c46fb115924d069e
JJong-Min <sambujakkk@naver.com>
2022-01-06 03:21:06 +0900

    [add new algorithm] merge_sort

--- [Week05~]Additional PS/[sort_algorithm] merge_sort.py
+++ [Week05~]Additional PS/[sort_algorithm] merge_sort.py
@@ -20,7 +20,50 @@ def merge_sort(arr):
     return merged_arr
 
 
+def mergeSort(myList):
+    if len(myList) > 1:
+        mid = len(myList) // 2
+        left = myList[:mid]
+        right = myList[mid:]
+
+        # Recursive call on each half
+        mergeSort(left)
+        mergeSort(right)
+
+        # Two iterators for traversing the two halves
+        i = 0
+        j = 0
+        
+        # Iterator for the main list
+        k = 0
+        print(k, left, right)
+        print('before sort', myList)
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+              # The value from the left half has been used
+              myList[k] = left[i]
+              # Move the iterator forward
+              i += 1
+            else:
+                myList[k] = right[j]
+                j += 1
+            # Move to the next slot
+            k += 1
+
+        # For all the remaining values
+        while i < len(left):
+            myList[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            myList[k]=right[j]
+            j += 1
+            k += 1
+        print('after sort', myList)
+            
 arr = [1, 3, 2, 5, 4, 7, 9, 8, 10]
-print(merge_sort(arr))
+mergeSort(arr)
+print(arr)
 
 
