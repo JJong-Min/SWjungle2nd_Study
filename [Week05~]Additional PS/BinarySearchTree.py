a7fea027f55bd0a9ddfc7c9c6d52e8383cc53c88
JJong-Min <sambujakkk@naver.com>
2022-01-06 10:31:09 +0900

    [modified] BinarySearchTree

--- [Week05~]Additional PS/BinarySearchTree.py
+++ [Week05~]Additional PS/BinarySearchTree.py
@@ -8,76 +8,122 @@ class BinarySearchTree:
     def __init__(self):
         self.root = None
 
-    def insert(self, value):
-        if self.root is None:
-            self.root = Node(value)
-            return
-        
-        current_node = self.root
-        while True:
-            if value < current_node.value:
-                if current_node.left is not None:
-                    current_node = current_node.left
-                else:
-                    current_node.left = Node(value)
-                    break
-            else:
-                if currenct_node.right is not None:
-                    current_node = current_node.right
-                else:
-                    current_node.right = Node(value)
-                    break
+    def search(self, key):
+        p = self.root
 
-    def search(self, value):
-        current_node = self.root
         while True:
-            if current_node is None:
+            if p is None:
+                return None
+            if key == p.value:
+                return p.value
+
+            elif key < p.value:
+                p = p.left
+
+            else:
+                p = p.right
+
+    def insert(self, key):
+
+        def insert_node(node, key):
+            if node.value == key:
                 return False
 
-            if value == current_node.value:
-                return True
-            
-            if current_node.value > value:
-                current_node = current_node.left
+            if node.value > key:
+                if node.left is None:
+                    node.left = Node(key)
+                else:
+                    insert_node(node.left, key)
 
             else:
-                current_node = current_node.right
+                if node.right is None:
+                    node.right = Node(key)
+                else:
+                    insert_node(node.right, key)
+            return True
 
-    def remove(self, value):
-        current_node = self.root
-        parent = self.head
-        while current_node:
-            if current_node.value == value:
-                serarched = True
-                break
-            elif value < current_node.value:
-                parent = current_node
-                current_node = current_node.left
-            else:
-                parent = current_node
-                current_node = current_node.right
-                
-        if not searched:
+        if self.root is None:
+            self.root = Node(key)
+        else:
+            insert_node(self.root, key)
+
+    def delete(self, value):
+        if self.head is None:
             return False
 
-        if current_node.left is None and current_node.right is None:
+        node = self.head
+        parent = self.head
+        check = False
+
+        while node:
+            if value == node.value:
+                check = True
+                break
+            elif value < node.value:
+                parent = node
+                node = node.left
+            else:
+                parent = node
+                node = node.right
+
+        if not check:
+            return False
+
+        # Case1 No Child
+        if node.left is None and node.right is None:
             if value < parent.value:
                 parent.left = None
             else:
                 parent.right = None
 
-        elif current_node.left is not None and current_node.right is None:
+        # Case2 Have a One Child
+        elif node.left and node.right is None:
             if value < parent.value:
-                parent.left = current_node.left
+                parent.left = node.left
             else:
-                parent.right = current_node.left
+                parent.right = node.left
 
-        elif crrent_node.left is None and current_node.right is not None:
-            if vaule < parent.value:
-                parent.left = current_node.right
+        elif node.left is None and node.right:
+            if value < parent.value:
+                parent.left = node.right
             else:
-                parent.right = current_node.right
+                parent.right = node.right
 
+        # Case3 Have Two Child
+        elif node.left and node.right:
+            current, child = node, node.right
+
+            while child.left:
+                current, child = child, child.left
+
+            node.value = child.value
+
+            if current != node:
+                if child.right:
+                    current.left = child.right
+                else:
+                    current.left = None
+            else:
+                node.right = child.right
         
 
-        
+    def pre_order_traversal(self):
+        def _pre_order_traversal(root):
+            if root is None:
+                pass
+            else:
+                
+                _pre_order_traversal(root.left)
+                print(root.value)
+                _pre_order_traversal(root.right)
+        _pre_order_traversal(self.root)
+
+    
+
+binary_tree = BinarySearchTree()
+binary_tree.insert(30)
+binary_tree.insert(20)
+binary_tree.insert(10)
+binary_tree.insert(50)
+binary_tree.insert(40)
+binary_tree.pre_order_traversal()
